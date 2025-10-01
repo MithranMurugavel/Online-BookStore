4436cfb8a9b6739ef09e63b31984a386503e4616
MithranMurugavel <mithran7741@gmail.com>
2025-10-02 00:40:24 +0530

    first commit

---
+++ frontend/src/App.jsx
@@ -0,0 +1,20 @@
+
+import { Outlet } from 'react-router-dom'
+import './App.css'
+import Navbar from './components/Navbar'
+
+function App() {
+
+  return (
+    <>
+    <nav>Navbar</nav>
+    <Navbar/>
+    <main class='min-h-screen max-w-screen-2x1 mx-auto px-4 py-6 font-primary'>
+       <Outlet/>
+    </main>
+    <footer>Footer</footer>
+    </>
+  )
+}
+
+export default App
---
+++ frontend/src/components/Navbar.jsx
@@ -0,0 +1,69 @@
+import React, { useState } from "react";
+import { Link } from "react-router-dom";
+import { AiOutlineBars } from "react-icons/ai";
+import { FiSearch } from "react-icons/fi";
+import { LiaUserGraduateSolid } from "react-icons/lia";
+import { BsBalloonHeart } from "react-icons/bs";
+import { MdOutlineShoppingCart } from "react-icons/md";
+
+{/*import Avatar images*/}
+import avatar from "../assets/avatar.png"
+
+const navigation = [
+    {name: "Dashboard", href:"/user-dashboard"},
+    {name: "Orders", href:"/orders"},
+    {name: "Cart Page", href:"/cart"},
+    {name: "Check Out", href:"/checkout"},
+]
+export const Navbar = () => {
+
+   const [isDropdownOpen,setIsDropdownOpen]= useState(false)
+   console.log(isDropdownOpen)
+
+  const currentuser = true;
+  return (
+    <header className="max-w-screen-2xl mx-auto px-6 py-6">
+      <nav className="flex justify-between items-center">
+        {/*left side */}
+        <div className="flex items-center md:gap-16 gap-4">
+          <Link to="/">
+            <AiOutlineBars className="size-6" />
+          </Link>
+
+          <div className="relative sm:w-72 w-40 space-x-2">
+            <FiSearch className="absolute inline-block left-1.5 inset-y-2" />
+
+            <input
+              type="text"
+              placeholder="Search Here"
+              className="bg-[#EAEAEA] w-full py-1 md:px-8 px-6 rounded-md focus:outline-none"
+            />
+          </div>
+        </div>
+
+        {/*right side*/}
+        <div className="relative flex items-center md:space-x-3 space-x-2">
+          <div>
+            {
+              currentuser ? <>
+                <button onClick>
+                  <img src ={avatar} alt="Boken Image" className={`size-7 rounded-full ${currentuser ? 'ring-2 ring-blue-500' : ''}`}/>
+                </button></>:<LiaUserGraduateSolid className="size-6" to="/login"/>
+            }
+          </div>
+          <button>
+            <BsBalloonHeart className="size-9" />
+          </button>
+          <Link
+            to="/cart"
+            className="bg-primary p-1 sm:px-6 py-2 flex items-center rounded-lg"
+          >
+            <MdOutlineShoppingCart className="size-6" />
+            <span className="text-sm font-semibold sm:ml-1">Empty cart</span>
+          </Link>
+        </div>
+      </nav>
+    </header>
+  );
+};
+export default Navbar;
