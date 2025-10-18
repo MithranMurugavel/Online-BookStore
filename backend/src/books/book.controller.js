5a4a94d79db3b66a1be8fe68bc4ea36f4e22c485
MithranMurugavel <mithran7741@gmail.com>
2025-10-18 23:15:49 +0530

    feat:completed router and controllers

--- backend/src/books/book.controller.js
+++ backend/src/books/book.controller.js
@@ -12,18 +12,73 @@ const postABook = async(req, res) =>{
     }
 }
 
-const getAllBooks = async(req,res) =>{
+const getAllBooks = async(req, res) =>{
     try{
-        const books =await Book.find();
-        res.status(200).send({message:"posted succesfully",book:newBook})
+        const books =await Book.find().sort({ createdAt: -1});;
+        res.status(200).send({books})
     }
     catch(error){
-        console.log("Error fetching",error);
+        console.error("Error fetching",error);
         res.status(500).send({message:"Faild to fetch"})
     }
 }
 
+const SingleBook = async(req,res) =>{
+    try{
+        const {id} = req.params;
+        const book = await Book.findById(id)
+        if(!book){
+            res.status(404).send({message:
+                "Book not found"
+            })
+        }
+        res.status(200).send(book)
+    }
+    catch(error){
+        console.error(error);
+        res.status(500).send({message:"Error occured when fetching book"})
+    }
+}
+
+const UpdateBook =async (req,res) =>{
+    try {
+        const {id} = req.params;
+        const updateBook = await Book.findByIdAndUpdate(id,req.body,{new:true});
+       
+        if(!updateBook){
+            res.status(404).send({message:"Not found"})
+        }
+        else{
+             res.status(200).send({updateBook});
+        }
+    } catch (error) {
+        console.error("Error when updating")
+        res.status(500).send({message:error})
+    }
+}
+
+const DeleteBook =async (req, res) =>{
+    try {
+        const {id} =req.params;
+        const deleteBook = await Book.findByIdAndDelete(id);
+        if(!deleteBook){
+            res.status(404);
+            res.send("Page Not Found");
+        }
+        else{
+            res.status(200);
+            res.send({message:"Successfully Delete the book"})
+        }
+    } catch (error) {
+        console.error({error});
+        res.status(500).send({error})
+        
+    }
+};
 module.exports ={
     postABook,
-    getAllBooks
+    getAllBooks,
+    SingleBook,
+    UpdateBook,
+    DeleteBook
 }
--- backend/src/books/book.route.js
+++ backend/src/books/book.route.js
@@ -1,11 +1,12 @@
 const express = require('express');
-const { postABook, getAllBooks } = require('./book.controller');
+const { postABook, getAllBooks, SingleBook, UpdateBook, DeleteBook } = require('./book.controller');
 const router = express.Router();
 
+// frontend => backend server => controller => book schema  => database => send to server => back to the frontend
 //post book
 router.post("/create-book",postABook)
-//get all book
-router.post("/create-book",postABook)
-
 router.get("/",getAllBooks)
+router.get("/:id",SingleBook)
+router.put("/edit/:id",UpdateBook)
+router.delete("/delete/:id",DeleteBook);
 module.exports = router;
