8c7e7c2470ae9b824c8fd7791885ba3285ed5d7a
MithranMurugavel <mithran7741@gmail.com>
2025-11-02 22:10:33 +0530

    feat(jwt):Initialized the JWT Auth to admin

--- backend/src/users/user.route.js
+++ backend/src/users/user.route.js
@@ -1,13 +1,24 @@
 const express = require('express')
-
+const User =require('./user.model');
+const jwt = require('jsonwebtoken');
 const router = express.Router();
 
+const JWT_SECRET = process.env.JWT_SECRET_KEY
 router.post("/admin",async (req,res) =>{
     const {usernaem,password} = req.body;
 
     try {
         
-        const admin = await User.findOne({username})
+        const admin = await User.findOne({username});
+        if(!admin){
+            res.status(404).send({message:"Admin not found"})
+        }
+
+        if(username.password !== admin.password){
+            res.status(401).send({message:"Invalid passwlrd"});
+        }
+
+        const token = jwt.sign({id:admin._id,username:admin.username,role:admin.role},JWT_SECRET,{expiresIn:"1h"})
     } catch (error) {
         console.error("failed to login");
         res.status(401).send('failed')
